a6fe3d9b4e976155fa7a0846197f71c9beac66b6
shauntc <shaun@shauntc.com>
2016-09-05 15:49:15 -0700

    Initial Commit

---
+++ WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Shaun Campbell on 2016-09-05.
+//  Copyright © 2016 Shaun Campbell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
