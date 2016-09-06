5cea21df30ea79441bcbcb99eab122b5d2c0d7bc
shauntc <shaun@shauntc.com>
2016-09-05 17:10:38 -0700

    Built out 5 - respond

--- WordEffects/main.m
+++ WordEffects/main.m
@@ -25,42 +25,68 @@ int main(int argc, const char * argv[]) {
         
         do{
         
-            NSLog(@"What would you like to do with your string? \n1)Uppercase \n2)Lowercase\n3)Numberize\n4)Canadianize\n5)Respond\n");
+            NSLog(@"What would you like to do with your string? \n1)Uppercase \n2)Lowercase\n3)Numberize\n4)Canadianize\n5)Respond\n6)De-Space\n");
             fgets(&userChoice, 2, stdin);
         
         
-            NSLog(@"%c", userChoice);
+           // NSLog(@"%c", userChoice);
             
             userChoiceNumber = userChoice - '0';
             
-            NSLog(@"%d", userChoiceNumber);
+           // NSLog(@"%d", userChoiceNumber);
             
-            if(5 < userChoiceNumber || userChoiceNumber < 1)
+            if(6 < userChoiceNumber || userChoiceNumber < 1)
             {
-                NSLog(@"Invalid input, please enter a number between 1 and 5\n");
+                NSLog(@"Invalid input, please enter a number between 1 and 6\n");
             }
             
             
         
-        }while(5 < userChoiceNumber || userChoiceNumber < 1);
+        }while(6 < userChoiceNumber || userChoiceNumber < 1);
         
        switch (userChoiceNumber)
         {
+            //Make UPPER CASE
             case 1:
                 NSLog(@"%@", [inputString uppercaseString]);
                 break;
+                
+            //Make lower case
             case 2:
                 NSLog(@"%@", [inputString lowercaseString]);
                 break;
+                
+            //Numberize
             case 3:
                 NSLog(@"Numberize!");
                 break;
+                
+            //Canadianize
             case 4:
                 NSLog(@"%@", [inputString stringByAppendingString:@", eh?"]);
                 break;
+                
+            //Respond
             case 5:
-                NSLog(@"Well damn!");
+                if([inputString hasSuffix:@"?"])
+                {
+                    NSLog(@"I Dont Know");
+                }
+                else if ([inputString hasSuffix:@"!"])
+                {
+                    NSLog(@"Whoa, calm down!");
+                }
+                else
+                {
+                    NSLog(@"I'm not really sure what to do with this");
+                }
                 break;
+            
+                //De-Space
+            case 6:
+                NSLog(@"De-Space it");
+                break;
+                
             default:
                 NSLog(@"Something went wrong, get to default case");
         }
